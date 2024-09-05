bd4a125749cfa7f5759fe77cbcb8edf2f9417c79
Ruthskery <cyrilimperial12@gmail.com>
2024-09-05 23:03:52 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -69,3 +69,40 @@ class AttendanceApp:
         else:
             print("No Match", "No matching fingerprint found in the database.")
             self.speak("No matching fingerprint found.")
+
+Traceback (most recent call last):
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/__init__.py", line 20, in init
+    eng = _activeEngines[driverName]
+  File "/usr/lib/python3.11/weakref.py", line 136, in __getitem__
+    o = self.data[key]()
+KeyError: None
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/miko/Downloads/prolockv2/test/prolock_voice.py", line 631, in <module>
+    app = AttendanceApp(root)
+  File "/home/miko/Downloads/prolockv2/test/prolock_voice.py", line 46, in __init__
+    self.speech_engine = pyttsx3.init()
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/__init__.py", line 22, in init
+    eng = Engine(driverName, debug)
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/engine.py", line 30, in __init__
+    self.proxy = driver.DriverProxy(weakref.proxy(self), driverName, debug)
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/driver.py", line 50, in __init__
+    self._module = importlib.import_module(name)
+  File "/usr/lib/python3.11/importlib/__init__.py", line 126, in import_module
+    return _bootstrap._gcd_import(name[level:], package, level)
+  File "<frozen importlib._bootstrap>", line 1206, in _gcd_import
+  File "<frozen importlib._bootstrap>", line 1178, in _find_and_load
+  File "<frozen importlib._bootstrap>", line 1149, in _find_and_load_unlocked
+  File "<frozen importlib._bootstrap>", line 690, in _load_unlocked
+  File "<frozen importlib._bootstrap_external>", line 940, in exec_module
+  File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/drivers/espeak.py", line 9, in <module>
+    from . import _espeak, toUtf8, fromUtf8
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/drivers/_espeak.py", line 101, in <module>
+    Initialize = cfunc('espeak_Initialize', dll, c_int,
+  File "/home/miko/Documents/prolock/myenv/lib/python3.11/site-packages/pyttsx3/drivers/_espeak.py", line 15, in cfunc
+    return CFUNCTYPE(result, *atypes)((name, dll), tuple(aflags))
+AttributeError: 'NoneType' object has no attribute '_handle
+
